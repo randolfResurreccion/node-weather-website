99cf2e68cde1131d2ff1abe51865d6a2f81b6cfd
Randolf Resurreccion <randolfr@gmail.com>
2021-04-06 16:14:19 -0400

    remove console log

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,6 @@ const forecast = (latitude, longtitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location');
         } else {
-            console.log(body.current)
             callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out. Current humidity is ${body.current.humidity}%.`);
         }
     })
